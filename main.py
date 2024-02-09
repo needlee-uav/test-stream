d11b91bfdb808fbebb9b2d2d38832dea403f03bb
needlee-uav <contact@needlee.io>
2024-02-09 20:19:15 +0400

    Gcloud websocket test

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import eventlet
 import eventlet.wsgi
 eventlet.monkey_patch()
-
+import datetime
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
-socketio = SocketIO(app, CORS_ALLOW_ALL_ORIGINS=True, cors_allowed_origins='http://localhost:8080')
+socketio = SocketIO(app)
+
+global current_image
+current_image = ""
 
 @socketio.on("connect")
 def test_connect():
@@ -17,12 +20,14 @@ def test_connect():
 @socketio.on("test")
 def test(data):
     global current_image
+    #print(data[:10])
     current_image = data
     
 @socketio.on("image")
-def receive_image():
+def receive_image(image):
     global current_image
-    emit("processed_image", current_image)
+    print(f'time: {datetime.datetime.now()}/hash: {current_image[:10]}')
+    emit("processed_image", "data:image/jpeg;base64,"+current_image)
 
 @app.route("/")
 def index():
