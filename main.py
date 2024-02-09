ffc4ba693c1a0e28f9b9e3c839d52b48bd3e133c
needlee-uav <contact@needlee.io>
2024-02-09 17:25:55 +0400

    Gcloud basic test

--- main.py
+++ main.py
@@ -1,67 +1,20 @@
-import eventlet
-import eventlet.wsgi
-eventlet.monkey_patch()
+import datetime
 
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "secret!"
-socketio = SocketIO(app, CORS_ALLOW_ALL_ORIGINS=True)
-global current_image 
-current_image = None
 
-@socketio.on("connect")
-def test_connect():
-    print("Connected")
-    emit("my response", {"data": "Connected"})
-
-@socketio.on("test")
-def test(data):
-    global current_image
-    # image = base64_to_image("data:image/jpeg;base64,"+data)
-    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # frame_resized = cv2.resize(gray, (640, 360))
-
-    # encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-
-    # result, frame_encoded = cv2.imencode(".jpg", frame_resized, encode_param)
-
-    # processed_img_data = base64.b64encode(frame_encoded).decode()
-    print(data)
-    b64_src = "data:image/jpg;base64,"
-    processed_img_data = b64_src + data
-    current_image = processed_img_data
-    
-
-
-@socketio.on("image")
-def receive_image(image):
-    global current_image
-    # Decode the base64-encoded image data
-    #print(image)
-    # image = base64_to_image(image)
-
-    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # frame_resized = cv2.resize(gray, (640, 360))
-
-    # encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-
-    # result, frame_encoded = cv2.imencode(".jpg", frame_resized, encode_param)
-
-    # processed_img_data = base64.b64encode(frame_encoded).decode()
-
-    # b64_src = "data:image/jpg;base64,"
-    # processed_img_data = b64_src + processed_img_data
-    print(str(current_image)[:10])
-    emit("processed_image", current_image)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def root():
+    dummy_times = [
+        datetime.datetime(2018, 1, 1, 10, 0, 0),
+        datetime.datetime(2018, 1, 2, 10, 30, 0),
+        datetime.datetime(2018, 1, 3, 11, 0, 0),
+    ]
+
+    return render_template("index.html", times=dummy_times)
 
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, port=8080)
+    app.run(host="127.0.0.1", port=8080, debug=True)
