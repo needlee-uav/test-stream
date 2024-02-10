6fd5c88d8363cf0cc6842ee180f783defe077746
needlee-uav <contact@needlee.io>
2024-02-10 17:26:02 +0400

    Client handling on server

--- main.py
+++ main.py
@@ -1,34 +1,28 @@
 import eventlet
 import eventlet.wsgi
 eventlet.monkey_patch()
-import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app)
 
-global current_image
-current_image = ""
+client = ""
+
+@socketio.on("update_sid")
+def update_sid():
+    client = request.sid
+    print(f'client sid updated: {client}')
 
 @socketio.on("connect")
 def test_connect():
-    print("Connected")
-    emit("my response", {"data": "Connected"})
+    emit("conn_success", {"data": "Connected"})
 
-@socketio.on("test")
-def test(data):
-    global current_image
-    #print(data[:10])
-    current_image = data
+@socketio.on("stream")
+def stream(data):
+    emit("processed_image", "data:image/jpeg;base64," + data, room=client)
     
-@socketio.on("image")
-def receive_image(image):
-    global current_image
-    print(f'time: {datetime.datetime.now()}/hash: {current_image[:10]}')
-    emit("processed_image", "data:image/jpeg;base64,"+current_image)
-
 @app.route("/")
 def index():
     return render_template("index.html")
